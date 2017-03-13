e59c791998fbd84850d7837112febd5025be04e1
Avion <amnatek@gmail.com>
2017-03-13 09:28:14 -0700

    Nick's header file

--- CLI_VirusSimulation/CLI_VirusSimulation/Person.h
+++ CLI_VirusSimulation/CLI_VirusSimulation/Person.h
@@ -1,12 +1,15 @@
-/*
- * Person.h
- *
- *  Created on: Feb 24, 2017
- *      Author: nicholas.zimmerman
- */
+//
+//  Person.h
+//  VirusSimulation
+//
+//  Created by Nick Zimmerman on 3/12/17.
+//
+//
 
-#ifndef PERSON_H_
-#define PERSON_H_
+
+
+#ifndef Person_h
+#define Person_h
 
 #include <iostream>
 #include <string>
@@ -18,19 +21,61 @@ using namespace std;
 class Person
 {
 public:
-int health;
-int natImmunity;
-int strain;
-
-Person(int h, int nat, int s) {
-	health = h;
-	natImmunity = nat;
-	strain = s;
-}
-
-
+    int natImmunity;
+    int health;
+    int strain;
+    
+    //Default constructor
+    Person() {
+        natImmunity = rand() % 10;
+        
+        if(rand() % 100 <= 10)
+        {
+            health = 0;
+            strain = ((rand() % 4)+1);	//levels 1-4 are stain strengths; 0 means no strain multiplier
+        }
+        else {
+            health = 1;
+            strain = 0;
+        }
+    }
+    
+    //Constructor given health probablility
+    Person(double prob) {		//prob entered as 0-100; likelihood of disease at startup
+        
+        if(rand() % 101 <= prob)	//rand produces 0-100
+        {
+            health = 0;
+            strain = ((rand() % 4)+1);	//levels 1-4 are stain strengths; 0 means no strain multiplier
+        }
+        else {
+            health = 1;
+            strain = 0;
+        }
+    }
+    
+    Person(double prob, int intensity) {		//prob entered as 0-100; likelihood of disease at startup
+        
+        if(rand() % 101 <= prob)	//rand produces 0-100
+        {
+            health = 0;
+            strain = intensity;	//levels 1-4 are stain strengths; 0 means no strain multiplier
+        }
+        else {
+            health = 1;
+            strain = 0;
+        }
+    }
+    
+        //Full-control constructor
+//        Person(int nat,int h, int s) {
+//            natImmunity = nat;		//0-9
+//            health = h;			//0-3
+//            strain = s;			//0-4
+//        }
 };
 
+#endif /* Person_h */
 
 
-#endif /* PERSON_H_ */
+
